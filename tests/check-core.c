99b9aafb651f75d765eb39d7a19276143676b87c
Ikey Doherty <ikey@solus-project.com>
2017-11-23 11:08:03 +0000

    Add initial template build structure
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ tests/check-core.c
@@ -0,0 +1,60 @@
+/*
+ * This file is part of libuf.
+ *
+ * Copyright © 2017 Ikey Doherty
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#include <check.h>
+#include <stdlib.h>
+
+/**
+ * Standard helper for running a test suite
+ */
+static int uf_test_run(Suite *suite)
+{
+        SRunner *runner = NULL;
+        int n_failed = 0;
+
+        runner = srunner_create(suite);
+        srunner_run_all(runner, CK_VERBOSE);
+        n_failed = srunner_ntests_failed(runner);
+        srunner_free(runner);
+
+        return n_failed == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+static Suite *test_create(void)
+{
+        Suite *s = NULL;
+        TCase *tc = NULL;
+
+        s = suite_create(__FILE__);
+        tc = tcase_create(__FILE__);
+        suite_add_tcase(s, tc);
+
+        /* TODO: Add actual tests. */
+        return s;
+}
+
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv)
+{
+        return uf_test_run(test_create());
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
