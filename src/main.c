988a2fb228bf285e90f1cd2d4fae2944547f2c05
Ikey Doherty <ikey@solus-project.com>
2017-11-23 11:40:43 +0000

    Start working on a super trivial binary
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/main.c
@@ -0,0 +1,41 @@
+/*
+ * This file is part of libuf.
+ *
+ * Copyright © 2017 Ikey Doherty
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "map.h"
+#include "util.h"
+
+int main(__uf_unused__ int argc, __uf_unused__ char **argv)
+{
+        UfHashmap *map = NULL;
+
+        map = uf_hashmap_new(NULL, NULL);
+        if (!map) {
+                return EXIT_FAILURE;
+        }
+        uf_hashmap_free(map);
+        return EXIT_SUCCESS;
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
